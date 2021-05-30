6174775196f45e9cbc6bb15f59650165240e7a5e
RicardoJosuePerezAltamirano <ricardoj.altamirano@outlook.com>
2021-05-29 20:02:45 -0500

    init

---
+++ BlazorDemoAuthClient/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace BlazorDemoAuthClient
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var identity = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.Name, "Rik"),
+            new Claim("Departament","Recursos Humanoss")
+        },
+        Microsoft.IdentityModel.Claims.AuthenticationTypes.Basic);
+            
+            var user = new ClaimsPrincipal(identity);
+
+            return Task.FromResult(new AuthenticationState(user));
+        }
+    }
+}
---
+++ BlazorDemoAuthClient/Policies/HumanResourcePolicy.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemoAuthClient.Policies
+{
+    public static class HumanResourcePolicy
+    {
+        public const string CanUserForHumanResource = "CanUserForHumanResource";
+        public static AuthorizationPolicy IsHumanResourceEmployee()
+        {
+            return new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .RequireClaim("Departament", "Recursos Humanos")
+                .Build();
+                
+        }
+    }
+}
